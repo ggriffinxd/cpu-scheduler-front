7c0dd30b974c9e48e9ae244c8fdaac5ead6b61ad
Gabriel Almeida <115562119+ggriffinxd@users.noreply.github.com>
2025-11-23 20:45:59 -0300

    feat: enhance dashboard layout with Main and Footer components

--- src/app/[locale]/(public)/dashboard/page.tsx
+++ src/app/[locale]/(public)/dashboard/page.tsx
@@ -1,9 +1,13 @@
+import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import { Main } from "./components/main";
 
 function Dashboard() {
   return (
     <div className="bg-background">
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
