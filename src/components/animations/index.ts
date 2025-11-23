bfee67f6c013e77cc4ccdc50f5e4d4e995fb43ee
Gabriel Almeida <115562119+ggriffinxd@users.noreply.github.com>
2025-11-23 20:47:03 -0300

    feat: create index file for animated components to streamline exports

---
+++ src/components/animations/index.ts
@@ -0,0 +1,4 @@
+export { AnimatedText } from "./animated-text";
+export { AnimatedImage } from "./animated-image";
+export { AnimatedContainer, StaggeredContainer } from "./animated-container";
+export { AnimatedCard } from "./animated-card";
